3228935ec3b2c9a622f22e1a79498d5731a84f57
sakurahayato <s145608@stu.bunri-u.ac.jp>
2022-01-31 14:16:52 +0900

    その他の画面＆共通化

--- Views/post.php
+++ Views/post.php
@@ -3,34 +3,7 @@
 include_once('../config.php');
 //便利な関数を読み込む
 include_once('../util.php');
-///////
-//ツイート一蘭
-////////
-$view_tweets = [
-    [
-        'user_id' => 1,
-        'user_name' => 'taro',
-        'user_nickname' => '太郎',
-        'user_image_name' => 'sample-person.jpg',
-        'tweet_body' => '今プログラミングをしています。',
-        'tweet_image_name' => null,
-        'tweet_created_at' => '2021-08-15 14:00:00',
-        'like_id' => null,
-        'like_count'=> 0,
-    ],
-    [
-        'user_id' => 2,
-        'user_name' => 'jiro',
-        'user_nickname' => '次郎',
-        'user_image_name' => null,
-        'tweet_body' => 'コワーキングスペースをオープンしました！',
-        'tweet_image_name' => 'sample-post.jpg',
-        'tweet_created_at' => '2021-03-14 14:00:00',
-        'like_id' => 1,
-        'like_count' => 1,
-    ]
-    
-    ]; 
+
 
     
 
@@ -39,15 +12,15 @@ $view_tweets = [
 <html lang="ja">
 <head>
     <?php include_once('../Views/common/head.php'); ?>
-    <title>ホーム画面 / Twitterクローン</title>
-    <meta name="description" content="ホーム画面です">
+    <title>つぶやく画面 / Twitterクローン</title>
+    <meta name="description" content="つぶやく画面です">
 </head>
 <body class="home">
     <div class="container">
         <?php include_once('../Views/common/side.php'); ?>
         <div class="main">
             <div class="main-header">
-                <h1>ホーム</h1>
+                <h1>つぶやく</h1>
             </div>
             <!-- つぶやき投稿エリア -->
             <div class="tweet-post">
@@ -69,17 +42,7 @@ $view_tweets = [
             <!-- 仕切りエリア -->
             <div class="ditch"></div>
 
-            <!-- つぶやき一覧エリア -->
-            <?php if (empty($view_tweets)) :?>
-                <p class="p-3">ツイートがありません</P>
-            <?php else: ?>
-                
-                <div class="tweet-list">
-                <?php foreach($view_tweets as $view_tweet): ?>
-                    <?php include('../Views/common/tweet.php'); ?>
-                <?php endforeach; ?>   
-                </div>
-            <?php endif; ?>
+            
         </div>
     </div>
     <?php include_once('../Views/common/foot.php'); ?>
